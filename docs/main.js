34951fbb112b784aa05cb5ef38f560460e09edfb
mubanga90 <douwemdevries@gmail.com>
2024-02-22 21:10:11 +0100

    Example github page

---
+++ docs/main.js
@@ -0,0 +1,17 @@
+import Bento from '../dist/main.js';
+
+const bentoOptions = {
+    preferredCellSize: { x: 47, y: 33 },
+};
+
+const gridElement = document.querySelector('#grid');
+const containerElement = gridElement.parentElement;
+const _bentoInstance = new Bento(gridElement, bentoOptions);
+
+containerElement.addEventListener("mousemove", () => {
+    if ((containerElement.w && containerElement.w !== containerElement.offsetWidth) || (gridElement.h && gridElement.h !== gridElement.offsetHeight)) {
+        _bentoInstance.setLayout();
+    }
+    containerElement.w = containerElement.offsetWidth;
+    containerElement.h = containerElement.offsetHeight;
+});
